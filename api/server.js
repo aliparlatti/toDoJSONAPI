46f1776c03f05bcfb8a4b878df528f8edcb419e9
Ali PARLATTI <aliparlatti@gmail.com>
2023-09-05 13:54:50 +0300

    fix randomData

--- api/server.js
+++ api/server.js
@@ -1,26 +1,21 @@
+// See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const fs = require("fs");
-const db = require("./db.js")();
-
-// db.json dosyasını sadece bir kez oluştur
-if (!fs.existsSync("/tmp/db.json")) {
-    fs.writeFileSync("/tmp/db.json", JSON.stringify(db, null, 2));
-}
 
 server.use(middlewares);
-
+// Add this before server.use(router)
 server.use(
     jsonServer.rewriter({
         "/api/*": "/$1",
         "/blog/:resource/:id/show": "/:resource/:id",
     })
 );
-
-server.db = db;
-server.use(jsonServer.router("/tmp/db.json")); // Oluşturulan dosyayı kullan
-
+server.use(router);
 server.listen(3000, () => {
     console.log("JSON Server is running");
 });
+
+// Export the Server API
+module.exports = server;
